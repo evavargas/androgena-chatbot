baa32ec5c660ab8d5a97ec57b4d650f75a826e11
evavargas <eva.estrella96@gmail.com>
2020-06-13 15:52:04 -0500

    message

--- controllers/MessageController.py
+++ controllers/MessageController.py
@@ -35,7 +35,7 @@ class MessageController:
 
         self.create(message_data)
 
-        if message_data["message_text"].upper() != "CONVERSAR":
+        if message_data["message_text"].upper() != "CONVERSAR" || 'Hola':
             self.music_search(message_data["message_text"])
             response = requests.post(
                 "https://graph.facebook.com/v2.6/me/messages",
