eeb230eab152709205b03dbe08de5c38f90a61a4
Fernando Lira <fernando@datatrust.pe>
2020-06-13 11:28:15 -0500

    hola

--- controllers/MessageController.py
+++ controllers/MessageController.py
@@ -1,7 +1,4 @@
-import os
-import requests
-import random
-import json
+import os, requests, random, json
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -10,18 +7,26 @@ load_dotenv(dotenv_path=env_path)
 
 class MessageController:
     def send_response(self,data,app):
-        m_lists = ['Hola','Adios','Que tal?']
+        m_lists = ['Hola','Adios','Que tal?','Papelón']
 
         if data["object"] == "page":
             for entry in data["entry"]:
                 for messaging_event in entry["messaging"]:
                     if messaging_event.get("message"):
                         sender_id = messaging_event["sender"]["id"]
-                        recipient_id = messaging_event["recipient"]["id"]  
-                        message_text = messaging_event["message"]["text"] 
+                        message_id = messaging_event["message"]["mid"]  
+                        message_text = messaging_event["message"]["text"]
+                        time = messaging_event["timestamp"]
         
-        print(sender_id)
 
+        message_data = {
+            "sender_id" : sender_id,
+            "message_id" : message_id,
+            "message_text" : message_text,
+            "time" : time   
+        }
+
+        self.create(message_data)
 
         response = requests.post("https://graph.facebook.com/v2.6/me/messages",
 
@@ -33,3 +38,8 @@ class MessageController:
         "recipient": {"id": sender_id},
         "message": {"text": random.choice(m_lists)}
     }))
+
+    def create(self,data):
+       # sender = Sender.first_or_create(id_sender='John')
+       print(data)
+       #message = Messages.create(id_sender=data["sender_id"],id_message=data["sender_id"],time=data["sender_id"],text=data["message_text"])
